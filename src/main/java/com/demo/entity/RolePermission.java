cf65293cffb7c1165fa9d81886f811630ab3ce98
caiteng <caiteng@192.168.1.121>
2017-08-09 13:41:35 +0800

    提交
    Signed-off-by: caiteng <caiteng@192.168.1.121>

---
+++ src/main/java/com/demo/entity/RolePermission.java
@@ -0,0 +1,23 @@
+package com.demo.entity;
+
+public class RolePermission {
+    private Integer rId;
+
+    private Integer pId;
+
+    public Integer getrId() {
+        return rId;
+    }
+
+    public void setrId(Integer rId) {
+        this.rId = rId;
+    }
+
+    public Integer getpId() {
+        return pId;
+    }
+
+    public void setpId(Integer pId) {
+        this.pId = pId;
+    }
+}
